f56c34e2db7f42fae1d19266ee147ea2c994c4a5
Lian Guevara <liann.guevara@gmail.com>
2021-11-13 19:47:48 -0500

    crud moongose

--- index.ts
+++ index.ts
@@ -7,14 +7,20 @@ const main = async () => {
     await conectarBD();
 
     // CREAR UN USUARIO
-    await UserModel.create({
-    apellido: "Gomez",
-    correo: "gomez.j@mail.com",
-    identificacion: "12345",
-    nombre: "Jorge",
-    rol:  Enum_Rol.administrador,
+    // await UserModel.create({
+    // apellido: "Alvarez",
+    // correo: "alvarez.j@mail.com",
+    // identificacion: "45212",
+    // nombre: "Miguel",
+    // rol:  Enum_Rol.administrador,
 
-});
+    // })
+    // .then((u) => {
+    //     console.log('usuarios', u);
+    // })
+    // .catch((e) =>{
+    //     console.error('Error obteniendo los usuarios', e)
+    // });
 
     // // OBTENER LOS USUARIO
     // await UserModel.find()
@@ -24,5 +30,35 @@ const main = async () => {
     // .catch((e) =>{
     //     console.error('Error obteniendo los usuarios', e)
     // });
+
+    // EDITAR UN USUARIO
+    // await UserModel.findOneAndUpdate(
+    //     { correo: "ruiz.miguel@gmail.com"},
+    //     {
+    //         nombre: "Miguel",
+    //         apellido: "ruiz"
+    //     }
+    // )
+    //     .then((u) => {
+    //         console.log('usuario actualizado con éxito', u);
+    //     })
+    //     .catch((e) =>{
+    //         console.error('Error actualizando el usuarios', e)
+    //     });
+
+    // ELIMINAR UN USUARIO
+    await UserModel.findOneAndDelete(
+        { correo: "ruiz.miguel@gmail.com"},
+      
+    )
+        .then((u) => {
+            console.log('usuario eliminado con éxito', u);
+        })
+        .catch((e) =>{
+            console.error('Error eliminado el usuarios', e)
+        });
+
+
+
 };
 main();
