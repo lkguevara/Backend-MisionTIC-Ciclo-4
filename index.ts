5a48e3424ec7e8d580e07d8326cc0aaab5c08543
Lian Guevara <liann.guevara@gmail.com>
2021-11-13 20:22:35 -0500

    creación modelo proyectos

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
 import conectarBD from './db/db';
 import { UserModel } from './models/user'
 import { Enum_Rol } from './models/enums'
+import { ProjectModel } from './models/project';
 
 
 const main = async () => {
     await conectarBD();
 
-    // CREAR UN USUARIO
+    
+    
+    
+
+
+
+};
+main();
+
+
+// CRUD 
+
+// CREAR UN USUARIO
     // await UserModel.create({
     // apellido: "Alvarez",
     // correo: "alvarez.j@mail.com",
@@ -47,18 +60,33 @@ const main = async () => {
     //     });
 
     // ELIMINAR UN USUARIO
-    await UserModel.findOneAndDelete(
-        { correo: "ruiz.miguel@gmail.com"},
+    // await UserModel.findOneAndDelete(
+    //     { correo: "ruiz.miguel@gmail.com"},
       
-    )
-        .then((u) => {
-            console.log('usuario eliminado con éxito', u);
-        })
-        .catch((e) =>{
-            console.error('Error eliminado el usuarios', e)
-        });
+    // )
+    //     .then((u) => {
+    //         console.log('usuario eliminado con éxito', u);
+    //     })
+    //     .catch((e) =>{
+    //         console.error('Error eliminado el usuarios', e)
+    //     });
+
+// CRUD PROYECTOS
+// CREAR PROYECTO
+    // ProjectModel.create({
+    //     nombre: "Proyecto 3",
+    //     presupuesto: 500,
+    //     fechaInicio: Date.now(),
+    //     fechaFin: new Date("2022/11/10"),
+    //     lider: '6190575b808dcfa9c520711c',
+    // })
+
+    // BUSCAR UN PROYECTO
+    // const proyecto: any = await ProjectModel.find({nombre: 'Proyecto 2'}).populate('lider');
+    // console.log ('El proyecto es: ', proyecto);
+
+    // const lider = await UserModel.find({ _id: proyecto[0].lider });
+    // console.log('El lider del proyecto es: ', lider);
 
 
 
-};
-main();
